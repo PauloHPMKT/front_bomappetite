0397a485f8b2cd2bb3761bb534031eaf2505cf1f
Paulo Sergio <pauloserg861@gmail.com>
2023-12-16 18:12:06 -0300

    feat: implement form login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,6 @@
 import { Container, CardLogin } from "./styles"
 import logo from '../../assets/images/food-square-logo.png';
+import { FormLogin } from "../../components/Forms/FormLogin";
 
 export const Login = () => {
   return(
@@ -7,9 +8,9 @@ export const Login = () => {
       <CardLogin>
         <div className="fs-banner">
           <h2>Seja bem vindo ao</h2>
-          <img src={logo} />
-          Login
+          <img src={logo} alt="Logo Food Square" />
         </div>
+        <FormLogin />
       </CardLogin>
     </Container>
 
