1f197267472bc50f38ac211502cf7c9d21c4ea13
Paulo Sergio <pauloserg861@gmail.com>
2023-12-16 19:08:10 -0300

    style: adjust code style

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
-import { GlobalStyles } from './styles/GlobalStyles.ts'
+import { GlobalStyles } from './styles/GlobalStyles'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
